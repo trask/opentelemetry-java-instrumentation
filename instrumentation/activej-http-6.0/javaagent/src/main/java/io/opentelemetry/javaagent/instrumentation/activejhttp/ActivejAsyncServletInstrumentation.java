f64e6cbd157a0b53142737ca21eaaab82f88b4a1
otelbot <197425009+otelbot@users.noreply.github.com>
2025-05-16 20:02:43 +0000

    ./gradlew spotlessApply

--- instrumentation/activej-http-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/activejhttp/ActivejAsyncServletInstrumentation.java
+++ instrumentation/activej-http-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/activejhttp/ActivejAsyncServletInstrumentation.java
@@ -33,8 +33,7 @@ public class ActivejAsyncServletInstrumentation implements TypeInstrumentation {
 
   @Override
   public ElementMatcher<TypeDescription> typeMatcher() {
-    return hasSuperType(named("io.activej.http.AsyncServlet"))
-            .and(not(isInterface()));
+    return hasSuperType(named("io.activej.http.AsyncServlet")).and(not(isInterface()));
   }
 
   @Override

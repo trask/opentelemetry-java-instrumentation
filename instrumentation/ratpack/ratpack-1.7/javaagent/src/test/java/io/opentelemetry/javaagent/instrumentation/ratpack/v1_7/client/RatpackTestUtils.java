308b193e90dea7406a36acc711d3a2ff3834122b
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-11-02 13:47:28 -0800

    fix: Remove incorrect OS-specific logic in Ratpack 1.7 tests

--- instrumentation/ratpack/ratpack-1.7/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/ratpack/v1_7/client/RatpackTestUtils.java
+++ instrumentation/ratpack/ratpack-1.7/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/ratpack/v1_7/client/RatpackTestUtils.java
@@ -11,7 +11,6 @@ import io.opentelemetry.instrumentation.test.utils.PortUtils;
 import io.opentelemetry.instrumentation.testing.junit.http.HttpClientTestOptions;
 import java.net.URI;
 import java.nio.channels.ClosedChannelException;
-import org.junit.jupiter.api.condition.OS;
 import ratpack.http.client.HttpClientReadTimeoutException;
 
 public final class RatpackTestUtils {
@@ -25,20 +24,19 @@ public final class RatpackTestUtils {
     }
     if (isNonRoutableAddress(uri)) {
       if (exception instanceof ClosedChannelException) {
-        return OS.WINDOWS.isCurrentOs() ? exception : new PrematureChannelClosureException();
+        return new PrematureChannelClosureException();
       }
       return new ConnectTimeoutException("Connect timeout (PT2S) connecting to " + uri);
     }
-    if (OS.WINDOWS.isCurrentOs() && isUnopenedPort(uri)) {
+    if (isUnopenedPort(uri)) {
       return exception;
     }
     return exception;
   }
 
   public static String ratpackExpectedClientSpanNameMapper(URI uri, String method) {
-    // Windows still emits CONNECT spans when dialing non-routable addresses, while Linux uses the
-    // HTTP method. Toggle expectations accordingly to keep the tests portable.
-    if (OS.WINDOWS.isCurrentOs() && "GET".equals(method) && isNonRoutableAddress(uri)) {
+    // Both Windows and Linux emit CONNECT spans when dialing non-routable addresses.
+    if ("GET".equals(method) && isNonRoutableAddress(uri)) {
       return "CONNECT";
     }
     return HttpClientTestOptions.DEFAULT_EXPECTED_CLIENT_SPAN_NAME_MAPPER.apply(uri, method);
